9c8731eb82a4a9c811216b38384f80bff393f66d
ndzamboni <ndzamboni@gmail.com>
2024-06-21 17:47:41 -0400

    adding env and gitignore for env

--- db/db.mjs
+++ db/db.mjs
@@ -1,15 +1,20 @@
-// db.mjs
-import pg from 'pg';
-const { Client } = pg;
+import pkg from 'pg';
+const { Client } = pkg;
+import dotenv from 'dotenv';
 
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'employee_db',
-  password: 'Obi1k3n0bi1!',
-  port: 5432,
+// Load environment variables from .env file
+dotenv.config();
+
+const db = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-client.connect();
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
 
-export default client;
+export default db;
