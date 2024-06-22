6f7b0f2a2d2461c185793a208403317c1a66cd9a
ndzamboni <ndzamboni@gmail.com>
2024-06-22 09:32:59 -0400

    update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,79 +1,85 @@
-import readline from 'readline';
-import db from './db/db.mjs';
+import inquirer from 'inquirer';
 import { viewAllDepartments, addDepartment } from './queries/departments.mjs';
 import { viewAllRoles, addRole } from './queries/roles.mjs';
 import { viewAllEmployees, addEmployee, updateEmployeeRole } from './queries/employees.mjs';
-import figlet from 'figlet';
 import chalk from 'chalk';
+import figlet from 'figlet';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Create readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// List of chalk colors
+const chalkColors = [
+  chalk.red,
+  chalk.green,
+  chalk.yellow,
+  chalk.blue,
+  chalk.magenta,
+  chalk.cyan,
+  chalk.white,
+  chalk.gray
+];
 
-// Function to display ASCII art header
-function displayHeader() {
-  console.log(
-    chalk.blue(
-      figlet.textSync('Employee Tracker', {
-        font: 'Standard',
-        horizontalLayout: 'default',
-        verticalLayout: 'default'
-      })
-    )
-  );
+// Main menu options
+const mainMenuOptions = [
+  'View all departments',
+  'View all roles',
+  'View all employees',
+  'Add a department',
+  'Add a role',
+  'Add an employee',
+  'Update an employee role',
+  'Exit'
+];
+
+// Function to get a random chalk color
+function getRandomColor() {
+  return chalkColors[Math.floor(Math.random() * chalkColors.length)];
 }
 
-// Main menu function
-function mainMenu() {
-  displayHeader();  // Display the header before the menu
+// Randomize menu options with colors
+const coloredMenuOptions = mainMenuOptions.map((option, index) => ({
+  name: getRandomColor()(`${index + 1}. ${option}`),
+  value: index
+}));
 
-  rl.question(chalk.yellow('What would you like to do? (Type the number)\n') +
-              chalk.red('1. View all departments\n') +
-              chalk.green('2. View all roles\n') +
-              chalk.blue('3. View all employees\n') +
-              chalk.magenta('4. Add a department\n') +
-              chalk.cyan('5. Add a role\n') +
-              chalk.yellow('6. Add an employee\n') +
-              chalk.redBright('7. Update an employee role\n') +
-              chalk.white('8. Exit\n') +
-              '> ', async (answer) => {
-    switch (answer.trim()) {
-      case '1':
-        await viewAllDepartments();
-        break;
-      case '2':
-        await viewAllRoles();
-        break;
-      case '3':
-        await viewAllEmployees();
-        break;
-      case '4':
-        await addDepartment();
-        break;
-      case '5':
-        await addRole();
-        break;
-      case '6':
-        await addEmployee();
-        break;
-      case '7':
-        await updateEmployeeRole();
-        break;
-      case '8':
-        rl.close();
-        db.end();
-        return;
-      default:
-        console.log(chalk.red('Invalid option. Please select a number from the menu.'));
-        break;
+async function mainMenu() {
+  console.log(chalk.green(figlet.textSync('Employee Tracker')));
+
+  const { action } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: coloredMenuOptions
     }
+  ]);
 
-    // Recursively call mainMenu to display the menu again
-    mainMenu();
-  });
+  switch (action) {
+    case 0:
+      await viewAllDepartments();
+      break;
+    case 1:
+      await viewAllRoles();
+      break;
+    case 2:
+      await viewAllEmployees();
+      break;
+    case 3:
+      await addDepartment();
+      break;
+    case 4:
+      await addRole();
+      break;
+    case 5:
+      await addEmployee();
+      break;
+    case 6:
+      await updateEmployeeRole();
+      break;
+    case 7:
+      process.exit();
+  }
+  await mainMenu(); // Ensure the main menu is called again after each action
 }
 
-// Start the main menu
 mainMenu();
