5654b73d0bdbf25a8700d02595534cbd0d30fe6e
ndzamboni <ndzamboni@gmail.com>
2024-06-22 20:00:05 -0400

    deleted old index.js file. Working to add front end to the backend.
    Stopping point was adding the front and backend folders. need to continue to move files around and make sure routes are set

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+// frontend/script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch data from backend APIs and render content dynamically
+    console.log('Script loaded');
+  });
+  
