7ee4c7975945250059df7368c85b9b7e8866eb0b
ndzamboni <ndzamboni@gmail.com>
2024-06-27 22:41:55 -0400

    updated null for manager ID

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ const client = new Client({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
 
